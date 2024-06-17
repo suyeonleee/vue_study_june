c0e8b08c4151ae36ca2873b5785c4e46f7b7df18
suyeonleee <115603405+suyeonleee@users.noreply.github.com>
2024-06-17 22:40:01 +0900

    로그인 화면 디자인 #1
    잼썼당

---
+++ src/views/Login.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <el-form id="form" :label-position="labelPosition" label-width="100px" :model="loginForm">
+            <el-form-item label="ID">
+                <el-input v-model="loginForm.id"></el-input>
+            </el-form-item>
+            <el-form-item label="PWD">
+                <el-input type="password" v-model="loginForm.pwd"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button @click="onSubmit">LOG IN</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+<script>
+    export default {
+        name : 'LogIn', 
+        data() {
+            return {
+                labelPosition: 'left',
+                loginForm: {
+                    id: '',
+                    pwd: ''
+                }
+            };
+        },
+        methods : {
+            onSubmit() {
+                console.log(this.loginForm);
+            }
+        }
+    }
+
+</script>
+<style>
+    #form {
+        width: 300px;
+        margin: auto;
+        top:300px;
+    }
+</style>
